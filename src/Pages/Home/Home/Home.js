27e586ff1cae672217137e47016d9eb5b17572b0
razumolla <rajuahmed9344@gmail.com>
2022-04-15 12:06:31 +0600

    responsive with bootstrap in experts

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Experts from '../Experts/Experts';
 import ServiceAll from '../ServiceAll/ServiceAll';
 
 const Home = () => {
     return (
         <div>
             <ServiceAll></ServiceAll>
+            <Experts> </Experts>
         </div>
     );
 };
