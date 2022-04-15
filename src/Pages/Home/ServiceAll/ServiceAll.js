e02e0d90a399404031d6bde3230f61fa76d2b313
razumolla <rajuahmed9344@gmail.com>
2022-04-15 11:19:53 +0600

    responsive with mediaquery

--- src/Pages/Home/ServiceAll/ServiceAll.js
+++ src/Pages/Home/ServiceAll/ServiceAll.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Service from '../Service/Service';
+import './ServiceAll.css';
+// import repair1 from '../../../images/repair1.png'
 
-const services = [
-    { id: 1, name: 'oil change', price: 100, description: '', img: '' },
-]
+// const services = [
+//     { id: 1, name: 'oil change', price: 100, description: '', img: 'https://i.ibb.co/0MqcrpY/repair1.png' },
+// ]
+
 
 const ServiceAll = () => {
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setServices(data));
+    }, [])
     return (
-        <div>
-            <h3>this is services</h3>
+        <div className='container'>
+            <div className="row">
+                <h1 className=' text-primary text-center mt-5'> Our Services</h1>
+                <div className="services-container">
+                    {
+                        services.map(service => <Service
+                            key={service.id}
+                            service={service}
+                        >
+                        </Service>)
+                    }
+                </div>
+            </div>
         </div>
     );
 };
