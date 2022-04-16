41e14fdf1ad88afffaa9415f0ae647dafeae0f18
razumolla <rajuahmed9344@gmail.com>
2022-04-16 19:20:51 +0600

    Email Pass auth with react firebase hook

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,18 +1,33 @@
 import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
 
 const Login = () => {
     const emailRef = useRef('');
     const passwordRef = useRef('');
     const navigate = useNavigate();
 
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    if (user) {
+        navigate('/home');
+    }
+
     const handleSubmit = event => {
         event.preventDefault();
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
-        console.log(email, password);
+
+        signInWithEmailAndPassword(email, password);
     }
+
     const navigateRegister = event => {
         navigate("/register")
     }
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Register.css'
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init'
 
 const Register = () => {
 
-    // const [
-    //     createUserWithEmailAndPassword,
-    //     user,
-    //     loading,
-    //     error,
-    // ] = useCreateUserWithEmailAndPassword(auth);
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
     const navigate = useNavigate();
 
     const navigateLogin = () => {
         navigate('/login');
     }
 
-    // if (user) {
-    //     navigate('/home');
-    // }
+    if (user) {
+        navigate('/home');
+    }
 
 
     const handleRegister = event => {
@@ -27,7 +29,7 @@ const Register = () => {
         const email = event.target.email.value;
         const password = event.target.password.value;
 
-        // createUserWithEmailAndPassword(email, password);
+        createUserWithEmailAndPassword(email, password);
     }
     return (
         <div className='register-form'>
